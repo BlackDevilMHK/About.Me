b8bf5a038ed7ede9c28d6032d1fbd6d44c161aba
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 17:29:55 +0600

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/bash
 echo ' Hello guys'|lolcat
+echo ' '
+echo ' '
 echo ' I am Black Devil MHK'|lolcat
+echo ' '
+echo ' '
 echo ' My real name is Md. Mahodi Hasan Khan'|lolcat
+echo ' '
+echo ' '
 echo ' My code name is B2A6K D3V1L-MHK'|lolcat
+echo ' '
+echo ' '
 echo ' I am not a programer or cyber expert'|lolcat
+echo ' '
+echo ' '
 echo ' I am a normal facebook user & a learner'|lolcat
